e6643c5619b1c3a98e361c1cc5cf0b9811859025
Jean-Philippe Moffet <jeanphilippemoffet@gmail.com>
2022-08-22 19:32:05 -0400

    v12 CACHE NAME

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v11';
+const CACHE_NAME = 'static-cache-v12';
 //Add list of files to cache here.
 const FILES_TO_CACHE = ['index.html','apropos.html','menu.html','reservation.html','./css/style.css' , './assets/images/background1.png','./assets/images/burger-11.webp','./assets/images/burger-12.webp','./assets/images/burger-background.webp','./assets/images/burger-background2.webp','./assets/images/burger-icon.png','./assets/images/burger7.jpg','./assets/images/burger8.png','./assets/images/burger9.jpg','./assets/images/burger10.webp','./assets/images/chef1.jpg','./assets/images/chef2.jpg','./assets/images/chef3.jpg','./assets/images/chef4.jpg','./assets/images/chef5.jpg','./assets/images/chef6.jpg','./assets/images/hamburger1.jpg','./assets/images/hamburger2.jpg','./assets/images/hamburger3.jpg','./assets/images/hamburger4.jpg','./assets/images/hotdog.webp','./assets/images/mission.jpeg','./assets/images/planete.jpg','./assets/images/planete.webp','./assets/images/planete2.webp','./assets/images/planete3.jpg','./assets/images/restaurant.jpg','./assets/images/veganBurger.jpg'];
 self.addEventListener('install', (evt) => {
